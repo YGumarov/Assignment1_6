6b0a9b8a7f0b42a4514d009003e525db4060102a
YGumarov <yerassylgumarov@gmail.com>
2023-04-09 14:25:11 +0600

    Initial commit

---
+++ Assignment1_6/Program.cs
@@ -0,0 +1,15 @@
+﻿static int Power(int a, int n)
+{
+    if (n == 0)
+    {
+        return 1;
+    }
+    return a*Power(a,n-1);
+}
+
+int a,n;
+
+a = int.Parse(Console.ReadLine());
+n = int.Parse(Console.ReadLine());
+
+Console.WriteLine(Power(a,n));
